7696f5f3b39ed22cc8ad3e980d5ea0d68c3b8788
Filip Rak <filip.rak@ikol.com>
2023-04-17 19:24:05 +0200

    Cleanup

--- nb/custom.fmt/test.sh
+++ nb/custom.fmt/test.sh
@@ -1,16 +1,22 @@
 echo "Starting unit tests...";
 
+RED='\033[41m'         # Red
+GREEN='\033[42m'       # Green
+CL='\033[0m'
+
 for file_input in test/test[0-9][0-9].php; do
     test_name=$(basename "$file_input" .php);
     file_expected="test/$test_name.formatted.php"
     
     echo "Testing: $file_input $filename";
-    
-    java -jar build/bin/nb-fmt.jar $file_input -o test/out.test.php
 
-    if ! diff --color $file_expected test/out.test.php; then
-        echo "FAILED!";
+    rm -rf test/out.test.php;
+    touch test/out.test.php;
+    java -jar build/bin/nb-fmt.jar $file_input -o test/out.test.php;
+
+    if ! diff --color --strip-trailing-cr $file_expected test/out.test.php; then
+        echo -e "${RED}TEST FAILED!${CL}";
         exit 5;
     fi
-    echo "OK! All tests passed.";
 done
+echo -e "${GREEN}OK! All tests passed.${CL}";
